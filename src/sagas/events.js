984236dd4ebab8a5aced7834862c00e745aa9e53
Wilhelm Törnroos <vilkku@gmail.com>
2019-08-01 23:16:08 +0300

    Use HTTPS

--- src/sagas/events.js
+++ src/sagas/events.js
@@ -2,7 +2,7 @@ import { call, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* fetchEvents (action) {
-  const events = yield call(axios.get, 'http://vilkku.kapsi.fi/asm/proxy.php');
+  const events = yield call(axios.get, 'https://vilkku.kapsi.fi/asm/proxy.php');
   yield put({type: 'RECEIVE_EVENTS', data: events.data});
 }
 
